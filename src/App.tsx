df4e03ec47ddfbed1e55cd5eed9112fc8fb56480
Alex Weisberger <alex@relaypayments.com>
2022-01-09 21:25:03 -0500

    Get packages working

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // import React from 'react'; 
-import { Implementation, Server, Client } from "./recur";
+import { Implementation, Server, Client, RecurrenceRuleType } from "./recur";
 import { Formik, Form, Field } from "formik";
 
 const kernel = new Implementation(new Client(), new Server());
@@ -7,24 +7,32 @@ const kernel = new Implementation(new Client(), new Server());
 type FormValues = { amount: number, name: string };
 
 const App = () => {
-  // const handleSubmit = (values: FormValues) => {
-  //   const { amount, name } = values;
-  //   kernel.addRecurringTransaction({ amount, name, recurrence_rule: { type: RecurrenceRuleType.WEEKLY } })
-  // };
+  const handleSubmit = (values: FormValues) => {
+    const { amount, name } = values;
+    kernel.addRecurringTransaction({ amount, name, recurrence_rule: { type: RecurrenceRuleType.WEEKLY } })
+  };
 
   return (
     <div className="App">
       <Formik
         initialValues={{ amount: 0.0, name: "" }}
-        onSubmit={() => {}}
+        onSubmit={handleSubmit}
       >
         {({ isSubmitting }: { isSubmitting: boolean}) => (
           <Form>
             <Field type="number" name="amount" />
             <Field type="string" name="name" />
+            <button type="submit" disabled={isSubmitting}>
+               Submit
+           </button>
           </Form>
         )}
       </Formik>
+      {kernel.viewRecurringTransactions().map((rt) => {
+        return (
+          <p key={rt.name}>{rt.name}</p>
+        )
+      })}
     </div>
   );
 }
